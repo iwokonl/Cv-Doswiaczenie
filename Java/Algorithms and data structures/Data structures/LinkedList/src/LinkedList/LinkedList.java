756ea02d2635e07c51076c40a8ab3276f26389e6
Iwo Stanisławski <iwokonl@wp.pl>
2023-07-03 21:06:18 +0200

    Iwo

--- Java/Algorithms and data structures/Data structures/LinkedList/src/LinkedList/LinkedList.java
+++ Java/Algorithms and data structures/Data structures/LinkedList/src/LinkedList/LinkedList.java
@@ -1,4 +1,30 @@
 package LinkedList;
 
 public class LinkedList {
+    int value;
+    LinkedList next;
+    public LinkedList(int value){
+        this.value = value;
+        this.next = null;
+    }
+
+    public void add(int value){
+        LinkedList temp = this;
+
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = new LinkedList(value);
+    }
+
+    public void print(){
+        LinkedList temp = this;
+        while (temp != null){
+            if(temp.next != null)
+                System.out.print(temp.value + "-> ");
+            else
+                System.out.print(temp.value);
+            temp = temp.next;
+        }
+    }
 }
