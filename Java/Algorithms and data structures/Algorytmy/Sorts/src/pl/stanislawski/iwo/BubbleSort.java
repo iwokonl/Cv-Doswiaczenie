c194d1387c8a4739e1d9fdde92416f055c01ecc1
Iwo Stanisławski <iwokonl@wp.pl>
2023-07-03 02:57:04 +0200

    Iwo

--- Java/Algorithms and data structures/Algorytmy/Sorts/src/pl/stanislawski/iwo/BubbleSort.java
+++ Java/Algorithms and data structures/Algorytmy/Sorts/src/pl/stanislawski/iwo/BubbleSort.java
@@ -1,28 +1,23 @@
 package pl.stanislawski.iwo;
 
 public class BubbleSort {
-    public BubbleSort() {
-    }
-
-    public void bubbleSort(int[] arr) {
-        for(int i = 0; i < arr.length; ++i) {
-            int current = arr[0];
-            int index = 0;
-
-            for(int z = 0; z < arr.length - 1 - i; ++z) {
-                if (current > arr[z + 1]) {
-                    swap(arr, index, z + 1);
-                    ++index;
+    public  void bubbleSort(int[] arr){
+        int current;
+        int index;
+        for(int i = 0;i<arr.length;i++){
+            current = arr[0];
+            index = 0;
+            for(int z = 0;z<arr.length-1-i;z++){
+                if(current > arr[z+1]){
+                    swap(arr,index,z+1);
+                    index++;
                 }
             }
         }
-
     }
-
-    private static void swap(int[] arr, int index1, int index2) {
+    private static void swap(int arr[], int index1, int index2){
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
     }
 }
-
--- Java/Algorithms and data structures/Algorytmy/Sorts/src/pl/stanislawski/iwo/QuickSort.java
+++ Java/Algorithms and data structures/Algorytmy/Sorts/src/pl/stanislawski/iwo/QuickSort.java
@@ -1,31 +1,28 @@
-
 package pl.stanislawski.iwo;
 
 public class QuickSort {
-    public QuickSort() {
-    }
-
     public static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
             int pivotIndex = partition(arr, low, high);
+
             quickSort(arr, low, pivotIndex - 1);
             quickSort(arr, pivotIndex + 1, high);
         }
-
     }
 
     public static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
         int i = low - 1;
 
-        for(int j = low; j < high; ++j) {
+        for (int j = low; j < high; j++) {
             if (arr[j] < pivot) {
-                ++i;
+                i++;
                 swap(arr, i, j);
             }
         }
 
         swap(arr, i + 1, high);
+
         return i + 1;
     }
 
@@ -36,14 +33,9 @@ public class QuickSort {
     }
 
     public static void printArray(int[] arr) {
-        int[] var1 = arr;
-        int var2 = arr.length;
-
-        for(int var3 = 0; var3 < var2; ++var3) {
-            int num = var1[var3];
-            System.out.print("" + num + " ");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
-
         System.out.println();
     }
 }
