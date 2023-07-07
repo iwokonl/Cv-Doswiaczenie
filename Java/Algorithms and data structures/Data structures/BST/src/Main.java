ab925ad355e6cd96e6ef8abf82fc4cc858b3f118
Iwo <37576228+iwokonl@users.noreply.github.com>
2023-07-07 22:09:32 +0200

    iwo

--- Java/Algorithms and data structures/Data structures/BST/src/Main.java
+++ Java/Algorithms and data structures/Data structures/BST/src/Main.java
@@ -1,6 +1,15 @@
+import pl.stanislawski.iwo.BST;
 
 public class Main {
     public static void main(String[] args) {
+        BST temp = new BST(5);
+        temp.insert(3);
+        temp.insert(7);
+        temp.insert(2);
+        temp.insert(4);
+        temp.insert(6);
+        temp.insert(8);
+        System.out.println(temp.toString());
 
     }
 }
