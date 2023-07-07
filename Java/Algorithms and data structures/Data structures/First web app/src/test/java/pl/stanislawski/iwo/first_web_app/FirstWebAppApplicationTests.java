0fc2d277f8dd75de0321be5475e7a3f25026f250
Iwo <37576228+iwokonl@users.noreply.github.com>
2023-07-07 23:33:05 +0200

    First web app
    First web app in Java <3

---
+++ Java/Algorithms and data structures/Data structures/First web app/src/test/java/pl/stanislawski/iwo/first_web_app/FirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.stanislawski.iwo.first_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
