4ffde344b753c26a87fadefb7ba05f8b40aedfa2
Iwo Stanisławski <iwo.stanislawski@icloud.com>
2023-11-27 21:01:57 +0100

    Iwo

--- Python/pythonProject/main.py
+++ Python/pythonProject/main.py
@@ -1,13 +1,25 @@
-# This is a sample Python script.
+import numpy as np
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def convert(k, x):
-    x = x - k
-    if (x > 180):
-        x = x - 360
-    elif (x < -180):
-        x = x + 360
-    return x
+# Otwórz obraz
+a = Image.open("Bez tytułu.png")
 
-print(convert(0, 190))
+# Upewnij się, że obraz jest w trybie RGB
+if a.mode != 'RGB':
+    a = a.convert('RGB')
+
+# Konwertuj obraz na tablicę numpy
+a = np.asarray(a)
+
+# Podziel tablicę na trzy kanały
+tablica_r = a[:, :, 0]
+tablica_g = a[:, :, 1]
+tablica_b = a[:, :, 2]
+tablica_r = Image.fromarray(tablica_r)
+tablica_g = Image.fromarray(tablica_g)
+tablica_b = Image.fromarray(tablica_b)
+
+# Wyświetl każdy z kanałów
+tablica_r.show()
+tablica_g.show()
+tablica_b.show()
