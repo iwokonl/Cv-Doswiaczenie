2057b842cf632f75e0d16e210eaf41ba0b522ac8
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-01-13 22:03:12 +0100

    Iwo

--- Credits for subjects and subjects from the university/Grafika/wypełnianie kraw/untitled/src/Main.java
+++ Credits for subjects and subjects from the university/Grafika/wypełnianie kraw/untitled/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<int[]> kraw = new ArrayList<>();
+        kraw.add(new int[]{3, -1,0,0});
+        kraw.add(new int[]{3, -1,6,0});
+        kraw.add(new int[]{0,0,1,3});
+        kraw.add(new int[]{6,0,4,3});
+        kraw.add(new int[]{1,3,2,2});
+        kraw.add(new int[]{4,3,2,2});
+        int y = -1;
+        ArrayList<int[]> aktytab = new ArrayList<>();
+        while(aktytab.isEmpty()){
+            for (int i = 0; i < kraw.size(); i++) {
+                if (kraw.get(i)[1] == y) {
+                    aktytab.add(kraw.get(i));
+                    
+                    kraw.remove(i);
+                }
+            }
+        }
+
     }
 }
