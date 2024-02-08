96c2dd44c9ac161e0c21e19ca8891b326dd3cbc5
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-02-08 12:13:15 +0100

    Iwo

--- Wzorce Projektowe/Singleton/src/Singleton/SingletonWersjaAsync.java
+++ Wzorce Projektowe/Singleton/src/Singleton/SingletonWersjaAsync.java
@@ -1,7 +1,7 @@
 package Singleton;
 
 public class SingletonWersjaAsync {
-    private static volatile SingletonWersjaAsync singleton; // Zapisuje to zapisanie zmiennej w pamięci głównej a nie w
+    private static volatile SingletonWersjaAsync singleton; // Zapisuje to w zmiennej w pamięci głównej a nie w
     // pamięci podręcznej procesora.
     private String data;
     private SingletonWersjaAsync(String data){
@@ -11,7 +11,9 @@ public class SingletonWersjaAsync {
         SingletonWersjaAsync temp = singleton; // Robimy to ponieważ volatile powoduje zapisanie zmiennej w pamięci głównej
         // a nie z pamięci podręcznej procesora. Usprawnia to proces
         if (temp == null){
-            synchronized (SingletonWersjaAsync.class){
+            synchronized (SingletonWersjaAsync.class){ // synchronized dopuszcza w przypadku wielo wątkowości tylko do jednego
+                // użycia kodu. Jeśli synchronized byłoby w prefixie metody oznacza to, że cała metoda jest wstrzymana  a
+                // jest to zbędne bo jeśli byłby gotowy obiekt Singleton to blokowało by całą metodę.
                 if(temp == null){
                     singleton = temp = new SingletonWersjaAsync(data);
                 }
