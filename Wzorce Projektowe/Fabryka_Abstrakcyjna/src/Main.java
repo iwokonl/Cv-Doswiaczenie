8d1ad6e245e138973e6188263107535815137fcb
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-02-08 04:45:23 +0100

    Iwo

--- Wzorce Projektowe/Fabryka_Abstrakcyjna/src/Main.java
+++ Wzorce Projektowe/Fabryka_Abstrakcyjna/src/Main.java
@@ -1,5 +1,11 @@
+import FabrykaAbstrakcyjna.Pizza.Pizza;
+import FabrykaAbstrakcyjna.Pizzerie.Pizzeria;
+import FabrykaAbstrakcyjna.Pizzerie.WłoskaPizzeria;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pizzeria pizzeria = new WłoskaPizzeria();
+        Pizza serowa = pizzeria.zamówPizze("serowa");
+        System.out.println(serowa.toString());
     }
 }
